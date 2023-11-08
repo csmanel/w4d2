4c1ef68f9a486d2c76feb96d1cb4841da46849a6
csmanel <csmanel@gmail.com>
2023-11-08 14:18:11 -0500

    add moves method for slideable

--- practice-for-ch-ruby-chess-long-practice-main/slideable.rb
+++ practice-for-ch-ruby-chess-long-practice-main/slideable.rb
@@ -1,53 +1,38 @@
 module Slideable
 
-  #  HORIZONTAL_AND_VERTICAL_DIRS stores an array of horizontal and vertical directions
-    # what a piece moves in a given direction it's row and/or col should increment by some value (change by some value)
-    # ex: A piece starts at pos [3, 3] and it moves horizontally to the left 
-      # it's position changes to [3, 2]
-      # the row increased by 0 and col increased by -1
   HORIZONTAL_AND_VERTICAL_DIRS = [
-    [:dx, :dy], # left [0, -1]
-    [:dx, :dy], # right
-    [:dx, :dy], # up (vertical)
-    [:dx, :dy]  # down (vertical)
+    [-1, 0], # left 
+    [1, 0], # right
+    [0, -1], # up (vertical)
+    [0, 1]  # down (vertical)
   ].freeze
 
-  #  DIAGONAL_DIRS stores an array of diagonal directions
   DIAGONAL_DIRS = [
-    [:dx, :dy], # left + up [-1, 1]
-    [:dx, :dy], # right + up
-    [:dx, :dy], # left + down
-    [:dx, :dy]  # right + down
+    [-1, -1], # left + up 
+    [1, -1], # right + up
+    [-1, 1], # left + down
+    [1, 1]  # right + down
   ].freeze
-
 
   def horizontal_and_vertical_dirs
-    # getter for HORIZONTAL_AND_VERTICAL_DIRS
     @horizontal_and_vertical_dirs
   end
 
   def diagonal_dirs
-    # getter for DIAGONAL_DIRS
     @diagonal_dirs
   end
 
-  # should return an array of all the places a Piece can move to
   def moves
-    # create an array to collect moves
     collect_moves = []
-    # iterate over each of the directions in which a slidable piece can move
-    @horizontal_and_vertical_dirs.each_with_index do |dirs, i|
-      # ele0 = > [dirx, diry]
-      
-      # use the Piece subclass `#move_dirs` method to get the info on each direction 
+    move_dirs.each do |dx, dy|
+      result = grow_unblocked_moves_in_dir(dx, dy)
+      collect_moves << result
       # for each direction, collect all possible moves in that direction
       # add moves to our moves array
       #(use the `grow_unblocked_moves_in_dir` helper method)
-      
-      
     end
     # return the final array of moves (containing all possible moves in all directions)
-    collect_moves = []
+    collect_moves 
   end
 
   private
@@ -55,6 +40,7 @@ module Slideable
   def move_dirs
     # subclass implements this
     raise NotImplementedError # this only executes if we don't implement in subclass
+    # gets all possible moves based on direction 
   end
 
   # this helper method is only responsible for collecting the moves in a given direction. Not all directions
