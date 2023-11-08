a497664669d7213e85aa8e84c5f14fe5112dbab1
Jacob Tabor <jtabor214@gmail.com>
2023-11-08 12:10:10 -0500

    Completed #initialize and #moves for Piece class

--- practice-for-ch-ruby-chess-long-practice-main/piece.rb
+++ practice-for-ch-ruby-chess-long-practice-main/piece.rb
@@ -1,7 +1,12 @@
 class Piece
 
-  def initialize
-    
+  def initialize(color, board, position)
+    @color = color
+    @board = board
+    @position = []
   end
 
+  def moves
+    raise "Piece is missing #valid_move method"
+  end
 end
